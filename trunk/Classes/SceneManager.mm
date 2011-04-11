4b46188eec9b94cde83dd15be485f19130a63e53
lzy5818957@gmail.com <lzy5818957@gmail.com@0131942e-3ec9-1e2b-90f5-5bf879f4a33d>
2011-04-11 02:01:19 +0000

    MileStone 3

--- trunk/Classes/SceneManager.mm
+++ trunk/Classes/SceneManager.mm
@@ -4,6 +4,7 @@
 //
 //  Created by Zunyi Lin on 11-03-14.
 //  Copyright 2011 University Of Saskatchewan. All rights reserved.
+//  Note:This file is based on Internet OpenSource Code.
 //
 
 #import "SceneManager.h"
